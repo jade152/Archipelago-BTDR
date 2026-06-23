7c69fe9afae56facb408538992d3464c8aa9641e
alex <alex.r.leali@gmail.com>
2026-06-23 19:15:57 -0400

    added game name and summary

--- worlds/btdr/world.py
+++ worlds/btdr/world.py
@@ -4,3 +4,10 @@ from typing import Any
 from worlds.AutoWorld import World
 
 class BrandishTheDarkRevenantWorld(World):
+    """
+    Brandish: The Dark Revenant is an Action RPG developed by Nihon Falcom.
+    It is a remake of the PC-98 Game Brandish for the PSP.
+    The game follows the swordsman Ares as he adventures an underground world while being hunted the sorceress Dela.
+    """
+
+    game = "Brandish: The Dark Revenant"
